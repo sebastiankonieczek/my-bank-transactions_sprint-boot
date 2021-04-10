ff6b551c7ae2b2a20c340f684031570438dd1288
Sebastian Konieczek <23232301+sebastiankonieczek@users.noreply.github.com>
2021-04-10 16:41:49 +0200

    add c3p0 connection pool data source through auto configuration

--- src/main/java/spring_course/my_bank_transactions/spring_boot/MyBankTransactionsSprintBootApplication.java
+++ src/main/java/spring_course/my_bank_transactions/spring_boot/MyBankTransactionsSprintBootApplication.java
@@ -1,11 +1,21 @@
 package spring_course.my_bank_transactions.spring_boot;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class MyBankTransactionsSprintBootApplication
 {
+   @Bean
+   public InitializingBean runner( final DataSource dataSource )
+   {
+      return () -> System.out.println( "This is the datasource this Spring Boot project is using: " + dataSource );
+   }
+
    public static void main( String[] args )
    {
       SpringApplication.run( MyBankTransactionsSprintBootApplication.class, args );
